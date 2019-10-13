c2199d232e8cd8fd895ec7dfdb7ab9aa85f95f31
Leandra <leandra10-99@hotmail.com>
2019-10-13 17:56:09 -0500

    Home001

--- TuImpacto/src/app/app.module.ts
+++ TuImpacto/src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Route} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ConocenosComponent } from './conocenos/conocenos.component';
+import { InvolucrateComponent } from './involucrate/involucrate.component';
+
+
+const routes :Route []= [
+{path:'', component:InicioComponent},
+{path: 'conocenos', component:ConocenosComponent },
+{path: 'involucrate', component:InvolucrateComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ConocenosComponent,
+    InvolucrateComponent
+
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
   providers: [],
